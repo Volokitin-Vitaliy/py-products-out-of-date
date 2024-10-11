60963cde1a198f7a75e12e7acedaa816e3d57f3f
Fibonacci <volokitin.vitaliy@icloud.com>
2024-10-11 11:17:04 +0300

    Complated

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,33 @@
-# write your code here
+import unittest
+import datetime
+
+from app.main import outdated_products
+
+
+class TestOutdatedProducts(unittest.TestCase):
+    def test_deadline_expired(self) -> None:
+        products = [
+            {
+                "name": "salmon",
+                "expiration_date": datetime.date(2024, 10, 10),
+                "price": 600,
+            }
+        ]
+        outdated_products(products)
+        self.assertEqual(products[0]["name"], "salmon")
+
+    def test_multiple_products(self) -> None:
+        products = [
+            {
+                "name": "chicken",
+                "expiration_date": datetime.date(2024, 10, 16),
+                "price": 120
+            },
+            {
+                "name": "salmon",
+                "expiration_date": datetime.date(2024, 10, 10),
+                "price": 600,
+            }
+        ]
+        outdated_products(products)
+        self.assertEqual(products[0]["name"], "salmon")
