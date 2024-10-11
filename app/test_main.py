00dbfdf402cb785d8051acb0ccf45c730cd9368e
Fibonacci <volokitin.vitaliy@icloud.com>
2024-10-11 11:48:03 +0300

    Complated

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,72 @@
-import unittest
 import datetime
-
 from app.main import outdated_products
 
 
-class TestOutdatedProducts(unittest.TestCase):
-    def test_deadline_expired(self) -> None:
-        products = [
-            {
-                "name": "salmon",
-                "expiration_date": datetime.date(2024, 10, 10),
-                "price": 600,
-            }
-        ]
-        outdated_products(products)
-        self.assertEqual(products[0]["name"], "salmon")
+def test_outdated_products_empty_list() -> None:
+    products = []
+    expected_outdated = []
+    assert outdated_products(products) == expected_outdated
 
-    def test_multiple_products(self) -> None:
-        products = [
-            {
-                "name": "chicken",
-                "expiration_date": datetime.date(2024, 10, 16),
-                "price": 120
-            },
-            {
-                "name": "salmon",
-                "expiration_date": datetime.date(2024, 10, 10),
-                "price": 600,
-            }
-        ]
-        outdated_products(products)
-        self.assertEqual(products[0]["name"], "salmon")
+
+def test_outdated_products_single_outdated() -> None:
+    today = datetime.date.today()
+    products = [
+        {
+            "name": "salmon",
+            "expiration_date": today
+        },
+        {
+            "name": "chicken",
+            "expiration_date": today - datetime.timedelta(days=5)
+        },
+        {
+            "name": "duck",
+            "expiration_date": today - datetime.timedelta(days=10)
+        },
+    ]
+    expected_outdated = ["chicken", "duck"]
+    assert outdated_products(products) == expected_outdated
+
+
+def test_outdated_products_no_outdated() -> None:
+    today = datetime.date.today()
+    products = [
+        {
+            "name": "salmon",
+            "expiration_date": today + datetime.timedelta(days=1)
+        },
+        {
+            "name": "chicken",
+            "expiration_date": today + datetime.timedelta(days=5)
+        },
+    ]
+    expected_outdated = []
+    assert outdated_products(products) == expected_outdated
+
+
+def test_outdated_products_all_outdated() -> None:
+    today = datetime.date.today()
+    products = [
+        {
+            "name": "salmon",
+            "expiration_date": today - datetime.timedelta(days=2)
+        },
+        {
+            "name": "chicken",
+            "expiration_date": today - datetime.timedelta(days=7)
+        },
+    ]
+    expected_outdated = ["salmon", "chicken"]
+    assert outdated_products(products) == expected_outdated
+
+
+def test_outdated_products_expiration_yesterday() -> None:
+    today = datetime.date.today()
+    products = [
+        {
+            "name": "salmon",
+            "expiration_date": today - datetime.timedelta(days=1)
+        },
+    ]
+    expected_outdated = ["salmon"]
+    assert outdated_products(products) == expected_outdated
